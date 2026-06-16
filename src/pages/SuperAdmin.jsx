e9af8e339f57689fcbcf9afa87d09cc1ca4a747c
Elias-Deg <eliasdeg1641@gmail.com>
2026-06-16 15:05:32 +0300

    join

--- src/pages/SuperAdmin.jsx
+++ src/pages/SuperAdmin.jsx
@@ -21,6 +21,7 @@ import { useSettings } from "../context/SettingsContext";
 import FormModal from "../components/FormModal";
 import ConfirmModal from "../components/ConfirmModal";
 
+
 const SuperAdmin = () => {
   const { user, logout } = useAuth();
   const { t } = useSettings();
